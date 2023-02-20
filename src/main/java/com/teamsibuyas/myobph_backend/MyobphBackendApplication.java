bbb0bd451f957904c1620af73748ad1046bbdd74
GroupFour77 <sevenseven403@yahoo.com>
2023-02-20 15:02:21 +0800

    first commit

---
+++ src/main/java/com/teamsibuyas/myobph_backend/MyobphBackendApplication.java
@@ -0,0 +1,13 @@
+package com.teamsibuyas.myobph_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyobphBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyobphBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teamsibuyas/myobph_backend/MyobphBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamsibuyas.myobph_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyobphBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
